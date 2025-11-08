4b68089a2e206aec52268834cd0e4a7efa6936e8
de-scientist <gitaumark502@gmail.com>
2025-11-08 16:00:36 +0300

    display initial number and interpretation

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const minusButton = document.getElementById("minus-button")
 //initialize number variable
 let number = 1;
 
+//display initial number and interpretation
 numberElement.textContent = number;
-interpretationElement.textContent = `${number} is an odd number`
+interpretationElement.textContent = `${number} is an odd number`;
 
 plusButton.addEventListener("click", () => {
     number += 1;
