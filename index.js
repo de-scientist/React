c9ff7086f355e196784d9ec76a6aed31503c4bce
de-scientist <gitaumark502@gmail.com>
2025-11-08 16:07:36 +0300

    function to update interpretation based on whether the number is even or odd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //get elements from the DOM 
-const plusButton = document.getElementById("plus-button")
-const numberElement = document.getElementById("number-element")
-const interpretationElement = document.getElementById("interpretation")
-const minusButton = document.getElementById("minus-button")
+const plusButton = document.getElementById("plus-button");
+const numberElement = document.getElementById("number-element");
+const interpretationElement = document.getElementById("interpretation");
+const minusButton = document.getElementById("minus-button");
 
 //initialize number variable
 let number = 1;
@@ -11,22 +11,22 @@ let number = 1;
 numberElement.textContent = number;
 interpretationElement.textContent = `${number} is an odd number`;
 
+//function to update interpretation based on whether the number is even or odd
+function updateInterpretation() {
+    if (number % 2 === 0) {
+        interpretationElement.textContent = `${number} is an even number`;
+    } else {
+        interpretationElement.textContent = `${number} is an odd number`;
+    }
+}
+
 plusButton.addEventListener("click", () => {
     number += 1;
-    numberElement.textContent = number
-    if (number % 2 === 0) {
-        interpretationElement.textContent = `${number} is an even number`
-    } else {
-        interpretationElement.textContent = `${number} is an odd number`
-    }
+    numberElement.textContent = number;
 })
 
 minusButton.addEventListener("click", () => {
     number -= 1;
-    numberElement.textContent = number
-    if (number % 2 === 0) {
-        interpretationElement.textContent = `${number} is an even number`
-    } else {
-        interpretationElement.textContent = `${number} is an odd number`
-    }
+    numberElement.textContent = number;
+    
 })
